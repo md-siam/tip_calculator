a175665b17edad8cc319f993a79d2b4a095d7969
Md. Siam <md.siam03@gmail.com>
2022-05-04 04:05:09 +0600

    math_expression added

--- lib/app/view/home_page.dart
+++ lib/app/view/home_page.dart
@@ -37,7 +37,7 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   transparentContainer(140),
                   Container(
-                    height: 700,
+                    height: 770,
                     width: double.infinity,
                     decoration: containerDecoration,
                     child: Padding(
@@ -47,6 +47,24 @@ class _HomePageState extends State<HomePage> {
                           Column(
                             children: [
                               Row(children: [Text('Bill', style: labelText)]),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 356,
+                                height: 46,
+                                alignment: Alignment.centerRight,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(7.0),
+                                ),
+                                child: const Text(
+                                  '142.55 ',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Color(0xFF00494D),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
                           const SizedBox(height: 30.0),
@@ -55,6 +73,115 @@ class _HomePageState extends State<HomePage> {
                               Row(children: [
                                 Text('Select Tip %', style: labelText)
                               ]),
+                              const SizedBox(height: 10),
+                              Wrap(
+                                spacing: 13,
+                                runSpacing: 13,
+                                children: [
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff00494d),
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      '5%',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff00494d),
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      '10%',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff00494d),
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      '15%',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff00494d),
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      '25%',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xff00494d),
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      '50%',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 156,
+                                    height: 46,
+                                    alignment: Alignment.centerRight,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(7.0),
+                                    ),
+                                    child: const Text(
+                                      'Custom ',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
                             ],
                           ),
                           const SizedBox(height: 30.0),
@@ -63,14 +190,31 @@ class _HomePageState extends State<HomePage> {
                               Row(children: [
                                 Text('Number of People', style: labelText)
                               ]),
+                              const SizedBox(height: 10),
+                              Container(
+                                width: 356,
+                                height: 46,
+                                alignment: Alignment.centerRight,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(7.0),
+                                ),
+                                child: const Text(
+                                  '5 ',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Color(0xFF00494D),
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
-                          const SizedBox(height: 30.0),
                           const SizedBox(height: 30.0),
                           Container(
                             height: 260,
                             decoration: BoxDecoration(
-                              color: const Color(0xff00494d),
+                              color: const Color(0xFF00494D),
                               borderRadius: BorderRadius.circular(20),
                             ),
                             child: Padding(
